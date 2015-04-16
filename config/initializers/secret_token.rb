470ee6f9da69319445f0a6c7a2e63def1e511ba2
App Academy Student <appacademy@Apps-Mac-mini-19.local>
2015-04-16 11:24:35 -0400

    Initialize App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicApp::Application.config.secret_key_base = '94cdc17854f3b681edc9c2b2c5a73d3b7361d1797b5f74934e3c165ab5e337d342c1255ff524437b95497d5f301fb96bee1d1d182e01980448064145f697a8c0'
