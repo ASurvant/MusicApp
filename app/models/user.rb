cffc6fb31975234cd5b740ef067fd67772fac9c3
App Academy Student <appacademy@Apps-Mac-mini-19.local>
2015-04-16 14:24:58 -0400

    User Model initialized

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,46 @@
-class User < ActiveRecord::Base
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
 
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    use.is_password?(password) ? user : nil
+  end
 
   def self.generate_session_token
-
+    SecureRandom::urlsafe_base64
   end
 
   def reset_session_token!
-
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
   end
 
-  def ensure_session_token
-
+  def password=(password)
+    self.password_digest = BCrpyt::Password.create(password)
   end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+    def ensure_session_token
+      self.session_token ||= self.class.generate_session_token
+    end
 end
